5a8ec9090e415de5535c8fc7f551912035bbd444
kondziet <konrad.dabrowski54@gmail.com>
2024-02-09 12:57:35 +0100

    Add YankeeSplit class for splitting cycles

---
+++ spring-backend/src/main/kotlin/pl/kondziet/springbackend/model/YankeeSplit.kt
@@ -0,0 +1,29 @@
+class YankeeSplit<T> {
+
+    fun split(cycle: List<T>, users: List<T>): List<List<T>>? {
+        if (users.isEmpty() || cycle.isEmpty()) {
+            return null
+        }
+
+        val results = mutableListOf<MutableList<T>>()
+        val indices = users.map { cycle.indexOf(it) }.sorted()
+
+        var start = 0
+        for (index in indices) {
+            results.add(cycle.subList(start, index + 1).toMutableList())
+            start = index + 1
+        }
+        results.add(cycle.subList(start, cycle.size).toMutableList())
+
+        val last = results.last()
+        if (last.size > 1) {
+            last.removeAt(last.lastIndex)
+            last.addAll(results.first())
+            results.remove(results.first())
+        } else {
+            results.removeAt(results.lastIndex)
+        }
+
+        return results
+    }
+}
