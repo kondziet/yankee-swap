594ce7856538e13561bc16afdf12715a20f6d608
Konrad Dąbrowski <86203980+kondziet@users.noreply.github.com>
2024-02-10 14:17:40 +0100

    Add support for multiple regular draw strategy in GroupService

--- spring-backend/src/main/kotlin/pl/kondziet/springbackend/application/service/GroupService.kt
+++ spring-backend/src/main/kotlin/pl/kondziet/springbackend/application/service/GroupService.kt
@@ -3,8 +3,10 @@ package pl.kondziet.springbackend.application.service
 import pl.kondziet.springbackend.domain.algorithm.SingleRegularDrawStrategy
 import org.springframework.stereotype.Service
 import pl.kondziet.springbackend.application.service.dto.GroupRequest
+import pl.kondziet.springbackend.domain.algorithm.MultipleRegularDrawStrategy
 import pl.kondziet.springbackend.infrastructure.persistence.GroupRepository
 import pl.kondziet.springbackend.domain.model.Draw
+import pl.kondziet.springbackend.domain.model.User
 
 @Service
 class GroupService(val groupRepository: GroupRepository, val drawService: DrawService) {
@@ -12,7 +14,13 @@ class GroupService(val groupRepository: GroupRepository, val drawService: DrawSe
     fun createGroup(groupRequest: GroupRequest) {
         val savedGroup = groupRepository.save(groupRequest.toGroup())
 
-        val drawResults = drawService.calculateDraws(savedGroup.toGraph(), SingleRegularDrawStrategy())
+        val drawStrategy = if (savedGroup.allowMutualDrawing) {
+            MultipleRegularDrawStrategy<User>()
+        } else {
+            SingleRegularDrawStrategy<User>()
+        }
+
+        val drawResults = drawService.calculateDraws(savedGroup.toGraph(), drawStrategy)
 
         savedGroup
             .copy(draws = listOf(Draw(drawResults)))
