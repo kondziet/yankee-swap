dab068838fb9a829d087a5fa04406fab795eaa8a
kondziet <konrad.dabrowski54@gmail.com>
2024-02-11 23:32:23 +0100

    Add GroupService method to get members draws

--- spring-backend/src/main/kotlin/pl/kondziet/springbackend/application/service/GroupService.kt
+++ spring-backend/src/main/kotlin/pl/kondziet/springbackend/application/service/GroupService.kt
@@ -1,30 +1,45 @@
 package pl.kondziet.springbackend.application.service
 
-import pl.kondziet.springbackend.domain.algorithm.SingleRegularDrawStrategy
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import pl.kondziet.springbackend.application.service.dto.GroupRequest
-import pl.kondziet.springbackend.domain.algorithm.MultipleRegularDrawStrategy
+import pl.kondziet.springbackend.application.service.dto.UserResponse
+import pl.kondziet.springbackend.domain.algorithm.*
 import pl.kondziet.springbackend.infrastructure.persistence.GroupRepository
 import pl.kondziet.springbackend.domain.model.Draw
 import pl.kondziet.springbackend.domain.model.User
+import pl.kondziet.springbackend.domain.model.toCycles
+import java.time.LocalDateTime
 
 @Service
 class GroupService(val groupRepository: GroupRepository, val drawService: DrawService) {
 
     fun createGroup(groupRequest: GroupRequest) {
-        val savedGroup = groupRepository.save(groupRequest.toGroup())
+        val group = groupRequest.toGroup()
 
-        val drawStrategy = if (savedGroup.allowMutualDrawing) {
+        val drawStrategy = if (group.allowMutualDrawing) {
             MultipleRegularDrawStrategy<User>()
         } else {
             SingleRegularDrawStrategy<User>()
         }
 
-        val drawResults = drawService.calculateDraws(savedGroup.toGraph(), drawStrategy)
+        val drawResults = drawService.calculateDraws(group.toGraph(), drawStrategy)
 
-        savedGroup
-            .copy(draws = listOf(Draw(drawResults)))
-            .let { groupRepository.save(it) }
+        groupRepository.save(
+            group.copy(
+                draws = listOf(Draw(results = drawResults, completedAt = LocalDateTime.now()))
+            )
+        )
+    }
+
+    fun getMembersDraws(groupId: String): List<UserResponse> {
+        val group = groupRepository.findByIdOrNull(groupId)
+            ?: throw IllegalArgumentException("Group with id $groupId not found")
+
+        if (group.shouldPerformYankeeSwap()){
+        }
+
+        return listOf()
     }
 
 }
