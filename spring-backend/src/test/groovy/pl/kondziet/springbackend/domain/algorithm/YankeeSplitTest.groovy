9cf0153d44ed35d5766c0fc1cc7265abf951ad5e
kondziet <konrad.dabrowski54@gmail.com>
2024-02-13 15:43:20 +0100

    Remove unnecessary test case

--- spring-backend/src/test/groovy/pl/kondziet/springbackend/domain/algorithm/YankeeSplitTest.groovy
+++ spring-backend/src/test/groovy/pl/kondziet/springbackend/domain/algorithm/YankeeSplitTest.groovy
@@ -19,24 +19,5 @@ class YankeeSplitTest extends Specification {
         ["Alice", "Bob", "David", "Carol", "Mark", "Alice"] | ["Alice", "Bob", "David", "Carol", "Mark"] | [["Alice"], ["Bob"], ["David"], ["Carol"], ["Mark"]]
     }
 
-    def "should throw exception"() {
-        when:
-        yankeeSplit.splitCycle(cycle, users)
-
-        then:
-        def exception = thrown(IllegalArgumentException)
-        exception.message == expectedExceptionMessage
-
-        where:
-        cycle                                               | users                              | expectedExceptionMessage
-        ["Alice", "Bob", "David", "Carol", "Mark", "Alice"] | []                                 | "Users cannot be empty"
-        []                                                  | ["Alice", "Bob", "David", "Carol"] | "Cycle cannot be empty"
-        ["Alice", "Alice"]                                  | ["Alice"]                          | "Cycle cannot be shorter than 3"
-        ["Alice", "Bob", "Alice"]                           | ["Alice", "Bob", "Mark"]           | "Cycle cannot be shorter than users"
-        ["Alice", "Bob", "David", "Carol", "Mark"]          | ["Alice", "David"]                 | "Cycle must be a cycle"
-        ["Alice", "Bob", "David", "Bob", "Alice"]           | ["Alice", "David"]                 | "Cycle cannot contain duplicates"
-        ["Alice", "Bob", "Alice"]                           | ["Alice", "David"]                 | "Cycle have to contain all users"
-        ["Alice", "Bob", "David", "Alice"]                  | ["Alice", "David", "Alice"]        | "Users cannot contain duplicates"
-
-    }
+    
 }
