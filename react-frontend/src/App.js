764109a99ad8225be51345ec79aed5fc5f794a98
kondziet <konrad.dabrowski54@gmail.com>
2024-02-15 20:22:16 +0100

    Generate React Project

---
+++ react-frontend/src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
