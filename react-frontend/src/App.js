57274cb4584246c729b73be1ff171d6646673d05
kondziet <konrad.dabrowski54@gmail.com>
2024-02-16 11:56:20 +0100

    Add routes for creating and viewing groups

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -7,6 +7,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/group">
+            <Route path="create" element={<div>Create Group</div>} />
+            <Route path=":groupId" element={<div>Group</div>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
--- react-frontend/src/components/Home.jsx
+++ react-frontend/src/components/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import background from "../img/home-background-2.jpg";
 import image from "../img/man-giving-present-woman.jpg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import publicClientRequest from "../api/ClientRequest";
 
 const Home = () => {
+  const [groupIdentifier, setGroupIdentifier] = useState("");
+  const navigate = useNavigate();
+
+  const handleGroupEnter = async () => {
+    try {
+      const response = await publicClientRequest.get(
+        `/group/${groupIdentifier}`,
+      );
+      const groupDraws = response.data;
+      navigate(`/group/${groupIdentifier}`, { state: { groupDraws } });
+    } catch (error) {
+      if (error.response.status === 404) {
+        alert("Group not found");
+      }
+    }
+  };
+
   return (
     <div
       className="flex h-screen w-screen flex-col items-center justify-center"
@@ -28,11 +46,16 @@ const Home = () => {
           </div>
           <div className="flex flex-col gap-2">
             <input
-              className="w-full rounded-lg bg-gray-200 p-4 text-4xl uppercase"
+              className="w-full rounded-lg bg-gray-200 p-4 text-4xl"
               type="text"
               placeholder="GROUP IDENTIFIER"
+              value={groupIdentifier}
+              onChange={(e) => setGroupIdentifier(e.target.value)}
             />
-            <button className="rounded-md bg-white px-8 py-2 text-2xl font-bold text-gray-500 hover:bg-gray-700">
+            <button
+              className="rounded-md bg-white px-8 py-2 text-2xl font-bold text-gray-500 hover:bg-gray-700"
+              onClick={handleGroupEnter}
+            >
               Enter
             </button>
           </div>
@@ -43,8 +66,13 @@ const Home = () => {
             src={image}
           ></img>
         </div>
-        <div className="rounded-xl bg-red-700 flex justify-center items-center">
-          <Link to="/group/create" className="font-bold text-4xl rounded-lg text-red-300 p-24">Create Group</Link>
+        <div className="flex items-center justify-center rounded-xl bg-red-700">
+          <Link
+            to="/group/create"
+            className="rounded-lg p-24 text-4xl font-bold text-red-300"
+          >
+            Create Group
+          </Link>
         </div>
       </div>
     </div>
