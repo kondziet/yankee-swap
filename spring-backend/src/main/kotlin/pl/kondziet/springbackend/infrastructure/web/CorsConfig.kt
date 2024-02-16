25f3faa76bbde7685b66fc880590413012101a6f
kondziet <konrad.dabrowski54@gmail.com>
2024-02-16 11:56:29 +0100

    Add CORS configuration for web requests

--- spring-backend/src/main/kotlin/pl/kondziet/springbackend/infrastructure/web/CorsConfig.kt
+++ spring-backend/src/main/kotlin/pl/kondziet/springbackend/infrastructure/web/CorsConfig.kt
@@ -1,4 +1,23 @@
 package pl.kondziet.springbackend.infrastructure.web
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
 class CorsConfig {
+
+    @Bean
+    fun webMvcConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+            }
+        }
+
+    }
 }
