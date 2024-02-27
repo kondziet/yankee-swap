a919953d6eda5debcf9bbc45c259ee07560437bd
kondziet <konrad.dabrowski54@gmail.com>
2024-02-27 10:51:26 +0100

    Update status code

--- spring-backend/src/main/kotlin/pl/kondziet/springbackend/infrastructure/web/GroupControllerAdvice.kt
+++ spring-backend/src/main/kotlin/pl/kondziet/springbackend/infrastructure/web/GroupControllerAdvice.kt
@@ -40,8 +40,8 @@ class GroupControllerAdvice {
     @ExceptionHandler(IllegalStateException::class)
     fun handleIllegalStateException(exception: IllegalStateException): ResponseEntity<Any> {
         val response = mapOf(
-            "status" to HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            "error" to HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
+            "status" to HttpStatus.BAD_REQUEST.value(),
+            "error" to HttpStatus.BAD_REQUEST.reasonPhrase,
             "message" to "Internal server error",
             "details" to exception.message
         )
